a6b3e767eb3acfd57f6341c411efdf1ed3ccac91
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2023-02-17 11:23:30 -0300

    react

--- reactecommerce/src/App.js
+++ reactecommerce/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 import MasterLayout from "./layouts/admin/Master.layout";
 import Home from './components/frontend/Home'
+import Login from "./components/frontend/auth/Login";
+import Register from "./components/frontend/auth/Register";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Router>
         <Routes>
         <Route exact path="/" element={<Home/>} />
+
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+
         <Route path="/admin/*" element={<MasterLayout />} />  
         </Routes>
       </Router>
